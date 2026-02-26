112d95ff33555a6d7d956db55630e39d5f1b333e
ashwath amudhan c a <ashwathamudhan10@gmail.com>
2026-02-26 13:01:56 +0530

    backend change

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
-
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = '/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
